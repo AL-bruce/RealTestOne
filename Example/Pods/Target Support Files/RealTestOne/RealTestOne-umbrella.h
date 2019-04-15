c098ecbd9ac500c5859c9f3f69e4950b9d9c2f6c
黄启山 <642848215@qq.com>
2019-04-15 14:58:48 +0800

    addcode

--- Example/Pods/Target Support Files/RealTestOne/RealTestOne-umbrella.h
+++ Example/Pods/Target Support Files/RealTestOne/RealTestOne-umbrella.h
@@ -10,6 +10,7 @@
 #endif
 #endif
 
+#import "ALView.h"
 
 FOUNDATION_EXPORT double RealTestOneVersionNumber;
 FOUNDATION_EXPORT const unsigned char RealTestOneVersionString[];
--- RealTestOne/Classes/ALView.m
+++ RealTestOne/Classes/ALView.m
@@ -10,12 +10,12 @@
 
 @implementation ALView
 
-/*
-// Only override drawRect: if you perform custom drawing.
-// An empty implementation adversely affects performance during animation.
-- (void)drawRect:(CGRect)rect {
-    // Drawing code
+-(instancetype)initWithFrame:(CGRect)frame{
+    self = [super initWithFrame:frame];
+    if (self) {
+        self.backgroundColor = [UIColor greenColor];
+    }
+    return self;
 }
-*/
 
 @end
